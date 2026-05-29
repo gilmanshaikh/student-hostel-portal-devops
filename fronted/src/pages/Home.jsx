b6c4b7f4431b97b445634271ce0aa7a729097c6f
GILMAN SHAIKH <gilman12005@gmail.com>
2026-05-29 16:18:26 +0530

    Update Home.jsx

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -1,20 +1,11 @@
-function App() {
+function Home() {
   return (
-    <div
-      style={{
-        background: "black",
-        color: "red",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "80px",
-        fontWeight: "bold"
-      }}
-    >
-      APP WORKING
+    <div style={{ background: "#1e293b", minHeight: "100vh", color: "white" }}>
+      
+      {/* Leave all the existing homepage content, navbar, and cards inside here */}
+      
     </div>
   );
 }
 
-export default App;
+export default Home;
